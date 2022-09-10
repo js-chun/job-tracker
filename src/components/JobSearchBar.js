b203772b6d560bcd14ab7d432fddd543aa827df8
Jisoo Chun <jisoo.chun@outlook.com>
2022-09-09 21:46:20 -0400

    added reset button to search bar

--- src/components/JobSearchBar.js
+++ src/components/JobSearchBar.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react"
 import Form from "react-bootstrap/Form"
 import Button from "react-bootstrap/Button"
+import Stack from "react-bootstrap/Stack"
 
 function JobSearchBar(props) {
 	const { filterChange } = props
@@ -15,19 +16,30 @@ function JobSearchBar(props) {
 		filterChange(searchInput)
 	}
 
+	const handleReset = () => {
+		filterChange("")
+		setSearchInput("")
+	}
+
 	return (
 		<>
 			<ion-icon name="filter"></ion-icon>
 
 			<Form className="d-flex mx-3 w-25" onSubmit={handleSubmit}>
-				<Form.Control
-					type="text"
-					placeholder="Search keywords..."
-					onChange={handleSearchChange}
-				/>
-				<Button type="submit" variant="secondary">
-					<ion-icon name="search"></ion-icon>
-				</Button>
+				<Stack direction="horizontal" gap={1}>
+					<Form.Control
+						type="text"
+						placeholder="Search keywords..."
+						value={searchInput}
+						onChange={handleSearchChange}
+					/>
+					<Button type="submit" variant="secondary">
+						<ion-icon name="search" size="small"></ion-icon>
+					</Button>
+					<Button variant="secondary" onClick={handleReset}>
+						<ion-icon name="backspace" size="small"></ion-icon>
+					</Button>
+				</Stack>
 			</Form>
 		</>
 	)
